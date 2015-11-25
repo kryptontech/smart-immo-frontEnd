5a26514cc60d3629e8dab890287ab81865fad031
Abdel veno vedi vici <Abdel veno vedi vici@GOD-PC.local>
2015-11-25 09:35:43 +0000

    Mise à jour Form-inscription.jsp Creation form-inscription.js

---
+++ smart-immo/src/main/webapp/resources/assets/js/form-inscription.js
@@ -0,0 +1,25 @@
+
+//------------------------------
+//Change Tabs
+//------------------------------
+$(document).ready(function(){
+
+	function mySelectUpdate(){
+		setTimeout(function (){
+			$('.mySelectBoxClass').trigger('update');
+		}, 500);
+	}
+	mySelectUpdate();
+
+	$('#fieldset2').addClass('none');
+	
+	$( "#option1" ).click(function() {
+		$('#fieldset2').addClass('none');							
+		mySelectUpdate();
+	});
+	$( "#option2" ).click(function() {
+		$('#fieldset2').removeClass('none');
+		mySelectUpdate();
+	});						
+	
+	});
